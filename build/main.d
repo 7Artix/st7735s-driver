b046d7e61eeef020bf25c3690cf965921626b9c2
Artix <artixzhang@icloud.com>
2025-05-31 18:32:29 +0800

    add .gitignore

--- build/main.d
+++
@@ -1,3 +0,0 @@
-build/main.o: src/main.cpp include/main.hpp include/st7735s.hpp
-include/main.hpp:
-include/st7735s.hpp:
--- build/st7735s.d
+++
@@ -1,2 +0,0 @@
-build/st7735s.o: src/st7735s.cpp include/st7735s.hpp
-include/st7735s.hpp:
